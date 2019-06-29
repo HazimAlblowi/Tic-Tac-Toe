c1da5bafa74673b2c9bf1aedb093481082d8796b
Hazim Alblowi <hazim.alblowi@gmail.com>
2019-06-29 19:10:55 +0300

    Build simple functional game

---
+++ js/game.js
@@ -0,0 +1,58 @@
+
+const cells = document.querySelectorAll('.cell');
+const cellsArray = [[0, 1, 2],
+[3, 4, 5],
+[6, 7, 8]]
+let turn = true;
+
+const checkGameOver = function () {
+    for (let i = 0; i < cellsArray.length; i++) {
+
+        if ((cellsArray[i][0] == cellsArray[i][1] && cellsArray[i][1] == cellsArray[i][2]) ||
+            (cellsArray[0][i] == cellsArray[1][i] && cellsArray[1][i] == cellsArray[2][i])) {
+            return true;
+        }
+    }
+
+    if ((cellsArray[0][0] == cellsArray[1][1] && cellsArray[1][1] == cellsArray[2][2]) ||
+        (cellsArray[0][2] == cellsArray[1][1] && cellsArray[1][1] == cellsArray[2][0])) {
+        return true;
+    }
+
+    console.log('return false')
+    return false;
+}
+
+const gameOver = function () {
+    console.log(`Player *** ${turn ? 'X' : 'O'} *** WON!`)
+}
+
+
+const play = function () {
+    const col = this.getAttribute('data-c');
+    const row = this.getAttribute('data-r');
+
+    if (turn) {
+        this.innerText = 'O';
+        cellsArray[row][col] = 'O';
+        turn = false;
+
+    } else {
+        this.innerText = 'X';
+        cellsArray[row][col] = 'X';
+        turn = true;
+    }
+    this.removeEventListener('click', play);
+
+    if (checkGameOver()) {
+        gameOver();
+    }
+
+
+}
+
+
+
+cells.forEach(cell => {
+    cell.addEventListener('click', play);
+});
