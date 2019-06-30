4a39e6643f3f1f594adcc85d90fb26d1986565ed
Hazim Alblowi <hazim.alblowi@gmail.com>
2019-06-30 10:58:43 +0300

    Add players Info blocks

--- js/game.js
+++ js/game.js
@@ -2,7 +2,18 @@
 const cells = document.querySelectorAll('.cell');
 const messageLog = document.querySelector('.messageLog');
 const restartButton = document.querySelector('.restartButton');
+const playersInfo = document.querySelectorAll('.players');
 
+
+const players = [{
+    name: 'Player 1',
+    token: 'X',
+    color: 'black'
+}, {
+    name: 'Player 2',
+    token: 'O',
+    color: 'black'
+}]
 let cellsArray = [[0, 1, 2],
 [3, 4, 5],
 [6, 7, 8]];
@@ -10,25 +21,32 @@ let cellsArray = [[0, 1, 2],
 let turn = true;
 let noOfTurns = 0;
 
+const showResult = function (tie) {
+    if (tie) {
+        messageLog.innerText = `TIE!`;
+    } else {
+        messageLog.innerText = `${turn ? players[1].name : players[0].name} won!`;
+    }
+}
+
 const checkGameOver = function () {
     for (let i = 0; i < cellsArray.length; i++) {
 
         if ((cellsArray[i][0] === cellsArray[i][1] && cellsArray[i][1] === cellsArray[i][2]) ||
             (cellsArray[0][i] === cellsArray[1][i] && cellsArray[1][i] === cellsArray[2][i])) {
-            messageLog.innerText = `Player ${turn ? 'X' : 'O'} won!`;
+            showResult();
             return true;
         }
     }
 
     if ((cellsArray[0][0] == cellsArray[1][1] && cellsArray[1][1] == cellsArray[2][2]) ||
         (cellsArray[0][2] == cellsArray[1][1] && cellsArray[1][1] == cellsArray[2][0])) {
-        messageLog.innerText = `Player ${turn ? 'X' : 'O'} won!`;
+        showResult();
         return true;
     }
 
     if (noOfTurns === 9) {
-        messageLog.innerText = `TIE!`;
-
+        showResult(true);
         return true;
     }
     return false;
@@ -48,15 +66,24 @@ const restart = function () {
         cell.addEventListener('click', play);
     });
 
-    messageLog.innerText = `Player ${turn ? 'O' : 'X'} trun!`
+    messageLog.innerText = `${turn ? players[0].name : players[1].name} trun!`
 
     cellsArray = [[0, 1, 2],
     [3, 4, 5],
     [6, 7, 8]];
 
-    cells.forEach ( cell => {
+    cells.forEach(cell => {
         cell.innerText = '';
     })
+
+    for (let i = 0; i < playersInfo.length; i++) {
+
+        playersInfo[i].querySelector('.name').innerText = players[i].name;
+        playersInfo[i].querySelector('.token').innerText = players[i].token;
+        playersInfo[i].querySelector('.color').innerText = players[i].color;
+
+    }
+
 }
 
 const play = function () {
@@ -66,13 +93,13 @@ const play = function () {
     noOfTurns++;
 
     if (turn) {
-        this.innerText = 'O';
-        cellsArray[row][col] = 'O';
+        this.innerText = players[0].token;
+        cellsArray[row][col] = 'X';
         turn = false;
 
     } else {
-        this.innerText = 'X';
-        cellsArray[row][col] = 'X';
+        this.innerText = players[1].token;
+        cellsArray[row][col] = 'O';
         turn = true;
     }
     this.removeEventListener('click', play);
@@ -82,7 +109,7 @@ const play = function () {
     if (checkGameOver()) {
         gameOver();
     } else {
-        messageLog.innerText = `Player ${turn ? 'O' : 'X'} trun!`
+        messageLog.innerText = ` ${turn ? players[0].name : players[1].name} trun!`
     }
 
 
